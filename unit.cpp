9ccd9766f66986ca8d79623021585ec668a354f8
Shreyas Sood <shreyassood@gmail.com>
2018-04-17 10:42:43 -0500

    Test number fix

--- unit.cpp
+++ unit.cpp
@@ -4,13 +4,14 @@
 int testno = 0;
 
 void check(int expected, int actual, const char* message) {
+    testno++;
     if(expected != actual) {
-        printf("FAILURE: Test %d with message: '%s'\n", ++testno, message);
+        printf("FAILURE: Test %d with message: '%s'\n", testno, message);
         exit(0);
     }
 }
 
 int main() {
-   check(0, 0, "constant check");
+   check(0xdeadbeef, 0xdeadbeef, "constant check");
    printf("SUCCESS: Passed %d tests\n", testno);
 }

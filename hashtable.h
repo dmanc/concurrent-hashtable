3abfd4d5c8c2cd6d4609f00a931e30fad89aa332
Daniel Mancia <dmancia@utexas.edu>
2018-04-17 22:13:11 -0500

    Hash function

--- hashtable.h
+++ hashtable.h
@@ -3,12 +3,12 @@
 
 class HashTable {
  public:
-    virtual int get(int) = 0;
+    virtual uint32_t get(uint32_t) = 0;
     virtual void put(int) = 0;
-    virtual int del(int) = 0;
-    virtual bool hasKey(int) = 0;
+    virtual uint32_t  del(uint32_t) = 0;
+    virtual bool hasKey(uint32_t) = 0;
 
-    int size() {
+    uint32_t size() {
         return entries;
     }
 
@@ -17,7 +17,12 @@ class HashTable {
     }
 
  protected:
-    int entries;
+    uint32_t entries;
 };
+
+// Knuth's Multiplicative Method
+uint32_t hash(uint32_t v) {
+    return v * UINT32_C(2654435761);
+}
 
 #endif

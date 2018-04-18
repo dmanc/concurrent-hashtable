516e8862ee111a4dcd363a975bfb658197f171cc
Daniel Mancia <dmancia@utexas.edu>
2018-04-17 23:48:20 -0500

    Basic bucket

--- hashtable.cpp
+++ hashtable.cpp
@@ -3,5 +3,16 @@
 
 int main() {
     std::cout << "Hello World\n";
+
+    std::cout << "Basic bucket test" << "\n";
+    Bucket b = Bucket();
+    b.add(1, 4);
+    b.add(2, 3);
+    std::cout << b.size << "\n";
+    Node* head = b.head;
+    while(head != NULL) {
+        std::cout << head->key << " " << head->value << "\n";        
+        head = head->next;
+    }
     return 0;
 }
--- sequential.h
+++ sequential.h
@@ -1,20 +1,45 @@
 #include "hashtable.h"
 
+class Node {
+  public:
+    uint32_t key, value;
+    Node* next;
+    Node() {}
+    Node(uint32_t K, uint32_t V): key(K), value(V){}
+};
+
+class Bucket {
+  public:
+    uint32_t size;
+    Node *head;
+    Bucket(): size(0), head(NULL) {}
+    void add(int k, int v) {
+        if(head == NULL) {
+            head = new Node(k, v);
+        } else {
+            Node *node = new Node(k, v);
+            node->next = head;
+            head = node;
+        }
+        size++;
+    }
+};
+
 class Sequential: public HashTable {
  public:
-    int get(int key) {
+    uint32_t get(uint32_t key) {
         return 0;
     }
 
-    void put(int val) {
+    void put(uint32_t val) {
         return;
     }
 
-    int del(int key) {
+    uint32_t del(uint32_t key) {
         return 0;
     }
 
-    bool hasKey(int key) {
+    bool hasKey(uint32_t key) {
         return 0;
     }
 

de19ac5eb3d0c9469647280b1ca21254d52032bc
Shreyas Sood <shreyassood@gmail.com>
2018-04-28 14:02:57 -0500

    Fixed deadlock

--- coarse.h
+++ coarse.h
@@ -14,31 +14,31 @@ class Coarse: public HashTable {
         Coarse() {}
         
         uint32_t get(uint32_t key) {
-            lock_guard<mutex> lock(lock);
+            lock_guard<mutex> lock_g(lock);
             uint32_t val = seq.get(key);
             return val;
         }
 
         void put(uint32_t key, uint32_t val) {
-            lock_guard<mutex> lock(lock);
+            lock_guard<mutex> lock_g(lock);
             seq.put(key, val);
         }
 
         bool hasKey(uint32_t key) {
-            lock_guard<mutex> lock(lock);
+            lock_guard<mutex> lock_g(lock);
             bool result = seq.hasKey(key);
             return result;
         }
 
         uint32_t size() {
-            lock_guard<mutex> lock(lock);
+            lock_guard<mutex> lock_g(lock);
             uint32_t result = seq.size();
             return result;
         }
 
         bool isEmpty() {
-            lock_guard<mutex> lock(lock);
-            return size() == 0;
+            lock_guard<mutex> lock_g(lock);
+            return seq.size() == 0;
         }
 };
 
